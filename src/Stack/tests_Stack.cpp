ddd9f5a9cafb4f4407c84a23711cefc8d83e3c4c
Adam Jeliński <adas.jelinski@gmail.com>
2022-03-11 01:51:46 +0100

    Support for organized tests

---
+++ src/Stack/tests_Stack.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+#include "Stack.h"
+
+TEST_CASE("stack_test_1")
+{
+    Stack stack;
+    stack.push(5);
+    REQUIRE(stack.pop() == 25);
+}
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,9 +1,3 @@
 #include <catch2/catch_test_macros.hpp>
-#include "Stack/Stack.h"
 
-TEST_CASE("stack_test_1")
-{
-    Stack stack;
-    stack.push(5);
-    REQUIRE(stack.pop() == 25);
-}
+// place for tests that can't be categorized
